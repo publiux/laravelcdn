2a882762323961ce2cee3ed0c64716d96162616c
Rehmat Alam <contact@rehmat.works>
2018-01-31 16:25:49 +0500

    Adding endpoint support to S3 provider

--- src/Publiux/laravelcdn/Providers/AwsS3Provider.php
+++ src/Publiux/laravelcdn/Providers/AwsS3Provider.php
@@ -48,6 +48,7 @@ class AwsS3Provider extends Provider implements ProviderInterface
                 's3' => [
                     'version' => null,
                     'region' => null,
+                    'endpoint' => null,
                     'buckets' => null,
                     'upload_folder' => '',
                     'http' => null,
@@ -66,7 +67,7 @@ class AwsS3Provider extends Provider implements ProviderInterface
      *
      * @var array
      */
-    protected $rules = ['version', 'region', 'key', 'secret', 'buckets', 'url'];
+    protected $rules = ['version', 'endpoint', 'region', 'key', 'secret', 'buckets', 'url'];
 
     /**
      * this array holds the parsed configuration to be used across the class.
@@ -134,6 +135,7 @@ class AwsS3Provider extends Provider implements ProviderInterface
             'threshold' => $this->default['threshold'],
             'version' => $this->default['providers']['aws']['s3']['version'],
             'region' => $this->default['providers']['aws']['s3']['region'],
+            'endpoint' => $this->default['providers']['aws']['s3']['endpoint'],
             'buckets' => $this->default['providers']['aws']['s3']['buckets'],
             'acl' => $this->default['providers']['aws']['s3']['acl'],
             'cloudfront' => $this->default['providers']['aws']['s3']['cloudfront']['use'],
@@ -226,6 +228,7 @@ class AwsS3Provider extends Provider implements ProviderInterface
             $this->setS3Client(new S3Client([
                         'version' => $this->supplier['version'],
                         'region' => $this->supplier['region'],
+                        'endpoint' => $this->supplier['endpoint'],
                         'http' => $this->supplier['http']
                     ]
                 )
